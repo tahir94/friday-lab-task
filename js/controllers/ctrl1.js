48853b6c0ec85d5991a5c3df56004137f60cb485
kashiffazal99 <kashiffazal99@gamil.com>
2015-06-12 20:54:57 +0500

    html file created

---
+++ js/controllers/ctrl1.js
@@ -0,0 +1,21 @@
+/**
+ * Created by 192.168.3.5 on 6/12/2015.
+ */
+
+
+  var app = angular.module("myApp",["ngMaterial"]);
+      app.controller("ctrl1",function($scope,$rootScope){
+           $rootScope.array = [];
+        $scope.user ={
+          
+        }
+
+
+         $scope.add = function()
+         {
+
+
+
+         };
+
+      });
